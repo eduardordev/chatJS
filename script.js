989d0c7c4eaef5b9cef9b9f208ae2549154f1407
eerh <eduardorh1312@gmail.com>
2021-02-04 20:48:36 -0600

    update

--- script.js
+++ script.js
@@ -13,15 +13,16 @@ var nxtln = document.createElement('br');
 let primaryColor = '#3A48B2';
 let secondColor = '#555765';
 let lightColor = '#C7CCED';
-let backColor = '#EBEBEB';
+let backColor = '#DFDFDF';
 
 window.onload = function(){
   createUser();
 
-  document.body.style.backgroundColor = "white";
+  //document.body.style.backgroundColor = "white";
   document.body.style.padding = "5px";
   document.body.style.fontFamily = "PT Sans";
   document.body.style.background="linear-gradient(45deg, rgba(27,94,215,1) 0%, rgba(43,233,206,1) 100%)";
+  document.body.style.height="650px";
 
 }
 
@@ -30,14 +31,15 @@ function createUser(){
 //login container
   var logContainer = document.createElement('div');
 
-  logContainer.style.border="1px solid #ACAEB6";
+  logContainer.style.border="0";
   logContainer.style.borderRadius="20px";
-  logContainer.style.backgroundColor=backColor;
+  logContainer.style.backgroundColor="white";
   logContainer.style.padding="60px";
   logContainer.style.position="absolute";
   logContainer.style.left="50%";
   logContainer.style.top="50%";
   logContainer.style.transform="translate(-50%, -50%)";
+  logContainer.style.alignContent="center";
 
   document.body.appendChild(logContainer);
 
@@ -47,26 +49,35 @@ function createUser(){
   headerLog.style.border="0";
   headerLog.style.borderRadius="20px 20px 0px 0px";
   headerLog.style.background="linear-gradient(90deg, rgba(0,212,255,1) 0%, rgba(11,81,208,1) 100%)";
-  headerLog.style.paddingTop="3px";
-  headerLog.style.paddingBottom="3px";
-  headerLog.style.padding="15px";
-  headerLog.style.paddingLeft="125px";
-  headerLog.style.paddingRight="125px";
-  headerLog.style.marginTop="-50px;"
+  headerLog.style.paddingTop="20px";
+  headerLog.style.paddingBottom="20px";
+  headerLog.style.marginLeft="-60px";
+  headerLog.style.marginRight="-60px";
+  headerLog.style.marginTop="-60px";
 
+  logContainer.appendChild(headerLog);
 
-
+//h3 with title
+  var logTxt = document.createElement('H3');
+  var textA = document.createTextNode("User Login");
+  logTxt.style.textAlign="center";
+  logTxt.style.fontWeight="bold";
+  logTxt.style.marginTop="35px";
+  logTxt.appendChild(textA);
+  logContainer.appendChild(logTxt);
 
 //input for the username
+
   var user = document.createElement('input');
 
   user.setAttribute("type", "text");
   user.setAttribute("placeholder", "Username");
-  user.style.fontfamily="calibri";
   user.style.padding="6px";
-  user.style.border="1px solid #ACAEB6"
+  user.style.margin="auto";
+  user.style.border="0";
   user.style.borderRadius="8px";
-  user.style.marginTop="15px";
+  user.style.backgroundColor=backColor;
+
   user.style.fontFamily="PT Sans";
 
   logContainer.appendChild(user);
@@ -86,7 +97,7 @@ function createUser(){
   logButt.style.paddingBottom="6px";
   logButt.style.marginRight="15px";
   logButt.style.marginLeft="15px";
-  logButt.style.marginTop="35px";
+  logButt.style.marginTop="25px";
   logButt.style.borderRadius="16px";
   logButt.style.color="white";
   logButt.style.fontFamily="PT Sans";
